2ab0794668c7af118b8a833b570a308025171cd4
rccssw <33927953+rccssw@users.noreply.github.com>
2019-07-02 12:51:50 +1000

    Add files via upload

---
+++ dev_test/rc_assets/main.js
@@ -0,0 +1,109 @@
+
+document.getElementById('app').innerHTML = `
+<div class="grey-panel">
+    <div id="column1" style="float:left; margin:0; width:782px;">
+		<div class="text">
+			<div class="section-heading-copy">Section Heading</div>
+			<div class="highlight-paragraph">Highlight paragraph duis aute irure dolor in reprehenderit in voluptate velit energy efficiency eu di fugiat.</div>
+			<div class="sed-ut-perspiciatis">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque  laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi ventus vitae ventus consecutem terra lora magnum lorem aqua.</div>
+		</div>
+
+		<div class="button01">
+			<div class="rectangle-2">
+				<div class="button-text">BUTTON</div>
+			</div>
+			<div class="rectangle-3">
+				<div class="plus">
+					<img src=./rc_assets/images/plus.svg></img>
+				</div>
+			</div>
+		</div>
+	
+		<div class="images">
+			<div class="image-leaves">
+				<img class ="image-left" src=./rc_assets/images/image-leaves.jpeg></img>
+			</div>
+		</div>
+
+		<div class="button02">
+			<div class="rectangle-2">
+				<div class="button-text">BUTTON</div>
+			</div>
+			<div class="rectangle-3">
+				<div class="plus">
+					<img src=./rc_assets/images/plus.svg></img>
+				</div>
+			</div>
+		</div>
+	
+    </div>
+
+    <div id="column2" style="float:left; margin:0;width:782px;">
+		<div class="icons">
+			<div class="icon-item-1">
+				<div class="01">
+					01
+				</div>
+				<div class="placeholder">
+					<div class="shape">
+						<img src=./rc_assets/images/cross.svg></img>
+					</div>
+				</div>
+				<div class="icon-label-lorem-ips">
+					Icon Label Lorem Ipsum Terra
+				</div>
+			</div>
+			<div class="icon-item-2">
+				<div class="02">
+					02
+				</div>
+				<div class="placeholder">
+					<div class="shape">
+						<img src=./rc_assets/images/cross.svg></img>
+					</div>
+				</div>
+				<div class="icon-label-lorem">
+					Icon Label Lorem
+				</div>
+			</div>
+			<div class="icon-item-3">
+				<div class="03">
+					03
+				</div>
+				<div class="placeholder">
+					<div class="shape">
+						<img src=./rc_assets/images/cross.svg></img>
+					</div>
+				</div>
+				<div class="icon-label-lorem-ips">
+					Icon Label Lorem Ipsum Terra
+				</div>
+			</div>
+			<div class="icon-item-4">
+				<div class="04">
+					04
+				</div>
+				<div class="placeholder">
+					<div class="shape">
+						<img src=./rc_assets/images/cross.svg></img>
+					</div>
+				</div>
+				<div class="icon-label">
+					Icon Label
+				</div>
+			</div>
+		</div>
+		<div class="image-foxgates">
+			<img src=./rc_assets/images/image-foxgates.jpeg></img>
+		</div>
+		<div class="image-ocean">
+			<img src=./rc_assets/images/image-ocean.jpeg></img>
+		</div>
+    </div>
+
+    <div id="column3" style="float:left; margin:0;width:194px;">
+	</div>	
+</div>
+
+	<p class="footer">Submitted by Raymond.</p>
+`
---
+++ dev_test/readme.md
@@ -0,0 +1,9 @@
+1) copy all files to a server directory say (say xampp/htdocs/dev_test
+
+2) run in the browser http://localhost/dev_test/index.html
+
+3) the pages will be served
+
+4) only large screen page at this stage
+
+5) font libraries not fully set up yet.
